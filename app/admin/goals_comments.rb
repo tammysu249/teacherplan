c27e39dc60fa451255047f76338cf95d8de9b19b
anonymous anonymous <tammy.su@kellogg.northwestern.edu>
2021-12-07 16:51:00 +0000

    Whitelist columns in GoalsComment for ActiveAdmin

--- app/admin/goals_comments.rb
+++ app/admin/goals_comments.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register GoalsComment do
 
+ permit_params :goal_id, :coach_id, :content
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
