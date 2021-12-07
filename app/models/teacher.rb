41f2a1004e0184af801f692358bc5d0e629bd849
anonymous anonymous <tammy.su@kellogg.northwestern.edu>
2021-12-07 16:50:26 +0000

    Generate teachers resource with devise

---
+++ app/models/teacher.rb
@@ -0,0 +1,18 @@
+class Teacher < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+  # Scopes
+
+  def to_s
+    first_name
+  end
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
---
+++ db/migrate/20211207165025_create_teachers.rb
@@ -0,0 +1,11 @@
+class CreateTeachers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :teachers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :school
+
+      t.timestamps
+    end
+  end
+end
