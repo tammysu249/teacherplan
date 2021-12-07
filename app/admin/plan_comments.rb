80ad3a9f79b2ffd8425216444294fa4c2793e4ab
anonymous anonymous <tammy.su@kellogg.northwestern.edu>
2021-12-07 16:51:02 +0000

    Whitelist columns in PlanComment for ActiveAdmin

--- app/admin/plan_comments.rb
+++ app/admin/plan_comments.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register PlanComment do
 
+ permit_params :improvement_plan_id, :coach_id, :content
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
