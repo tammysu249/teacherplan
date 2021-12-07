a2a1742efd4807193cbeb5487e4d6f0ffdd69bdd
anonymous anonymous <tammy.su@kellogg.northwestern.edu>
2021-12-07 16:50:54 +0000

    Whitelist columns in Invite for ActiveAdmin

--- app/admin/invites.rb
+++ app/admin/invites.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Invite do
 
+ permit_params :coach_id, :improvement_plan_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
