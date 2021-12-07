0f4092ebd42d77879789326e36977651ecf6acfc
anonymous anonymous <tammy.su@kellogg.northwestern.edu>
2021-12-07 16:50:50 +0000

    Whitelist columns in Teacher for ActiveAdmin

--- app/admin/teachers.rb
+++ app/admin/teachers.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Teacher do
 
+ permit_params :email, :password, :first_name, :last_name, :school
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
